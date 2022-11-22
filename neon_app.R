150ef503c1e04e38d6302ebf674b8fac96be3420
njlyon0 <nicholasjlyon@gmail.com>
2022-11-22 09:34:47 -0500

    First commit of core architecture of Shiny app (lacking any content but contains right libraries/headings)

---
+++ neon_app.R
@@ -0,0 +1,27 @@
+## ------------------------------------------ ##
+            # NEON Chemistry App
+## ------------------------------------------ ##
+
+# Housekeeping ----
+
+# Clear environment
+rm(list = ls())
+
+# Load libraries
+library(tidyverse); library(plotly) # Wrangling / plotting
+library(shiny); library(DT); library(shinyWidgets) # Shiny et al.
+
+# User Interface (UI) ----
+
+
+
+
+
+# Server ----
+
+
+
+
+
+
+# End ----
