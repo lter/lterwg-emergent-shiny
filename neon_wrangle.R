eba29b297072ef70af0b16231c621c4c036bcd9f
njlyon0 <nicholasjlyon@gmail.com>
2022-11-22 10:13:13 -0500

    Finished first pass at wrangling script

--- neon_wrangle.R
+++ neon_wrangle.R
@@ -19,8 +19,58 @@ data_v1 <- read.csv(file = file.path("data", "soilFieldChem.csv"))
 # Subset data to desired entries
 data_v2 <- data_v1 %>%
   dplyr::filter(nlcdClass %in% c("mixedForest", "deciduousForest",
-                "evergreenForest", "grasslandHerbaceous"))
+                "evergreenForest", "grasslandHerbaceous")) %>%
+  # Drop impossible coordinates
+  dplyr::filter(abs(Longitude) <= 180 & abs(Latitude) <= 90) %>%
+  # Fill NAs in other wanted columns with something more descriptive
+  dplyr::mutate(
+    siteID = ifelse(is.na(siteID), yes = "Not Recorded", no = siteID),
+    biophysicalCriteria = ifelse(is.na(biophysicalCriteria), 
+                                 yes = "Not Recorded", 
+                                 no = biophysicalCriteria),
+    sampleTiming = ifelse(is.na(sampleTiming), 
+                          yes = "Not Recorded", 
+                          no = sampleTiming)) %>%
+  # Coalesce seemingly duplicate columns
+  dplyr::mutate(
+    soilMoisture = dplyr::coalesce(soilMoisture, soilMoisture.1),
+    dryMassFraction = dplyr::coalesce(dryMassFraction.x,
+                                      dryMassFraction.y),
+    collectDate = dplyr::coalesce(collectDate.x, collectDate.y)
+  ) %>%
+  # Move these columns around
+  dplyr::relocate(dryMassFraction, .after = dryMassFraction.x) %>%
+  dplyr::relocate(collectDate, .after = collectDate.x) %>%
+  # Drop unwanted columns as well
+  dplyr::select(-X, -plotID.x, -namedLocation,
+                -dplyr::starts_with("coreCoordinate"),
+                -subplotID, -geodeticDatum, -collectDate.x,
+                -dplyr::ends_with("Uncertainty"),
+                -samplingProtocolVersion.x, -collectedBy,
+                -nTransBoutType.x, -eventID,
+                -sampleID, -sampleCode, -toxicodendronPossible,
+                -horizonDetails, -biomassID, -remarks,
+                -dataQF, -publicationDate.x,
+                -moistureSampleID, -dplyr::starts_with("oven"),
+                -soilMoisture.1, -smRemarks, -smMeasuredBy,
+                -smDataQF, -publicationDate.y, -cnSampleID,
+                -dplyr::starts_with("bgc"), -cnDataQF,
+                -cnLaboratoryName, -cnRemarks, -cnTestMethod,
+                -isoRemarks, -isoInstrument, -plotID.y,
+                -dryMassFraction.x, -dryMassFraction.y,
+                -collectDate.y,
+                -dplyr::starts_with("genetic"))
 
+# Check which columns were dropped
+setdiff(x = names(data_v1), y = names(data_v2))
 
+# Glimpse the data
+dplyr::glimpse(data_v2)
+
+# Export ----
+
+# Export broadly pared down data
+write.csv(x = data_v2, na = '', row.names = F,
+          file = file.path("data", "table_data.csv"))
 
 # End ----
