3338a21b44d8d245865078d3e752adfc6ff28352
njlyon0 <nicholasjlyon@gmail.com>
2022-11-22 11:06:45 -0500

    Removed remaining merge conflicts to create functioning app

--- app_graphs.R
+++ app_graphs.R
@@ -41,127 +41,77 @@ allsub <- soilFieldChem%>%
 ui <- fluidPage(
   titlePanel("Neon Graphs"),
   sidebarLayout(position = "left",
-                tabPanel("Graph",
-                         sidebarPanel(
-                           selectInput(inputId = "selection", 
-                                       label = h3("Select Type of Site"),
-                                       choices = c("All Sites", "Forrested Sites", "Grassland Sites"),
-                                       selected = 1),
-                           selectInput(inputId = "selection3",
-                                       label = h3("Soil Temp or Moisture"),
-                                       choices = c("soilTemp", "soilMoisture"),
-                                       selected = 1)),
-                         
-                         # mainPanel(plotOutput("distPlot2"))
-                         mainPanel(plotOutput("distPlot"))
-                ) ) )
+                sidebarPanel(
+                  selectInput(inputId = "selection", 
+                              label = h3("Select Type of Site"),
+                              choices = c("All Sites", "Forrested Sites", "Grassland Sites"),
+                              selected = 1),
+                  selectInput(inputId = "selection3",
+                              label = h3("Soil Temp or Moisture"),
+                              choices = c("soilTemp", "soilMoisture"),
+                              selected = 1)),
+                
+                # mainPanel(plotOutput("distPlot2"))
+                mainPanel(plotOutput("distPlot"))
+  ) )
 
 # Server ----
 server <- function(input, output) {
   
-                  output$select_s1 <- renderUI({
-                    
-                    selectizeInput('s1', 'Select variable 1', 
-                                   choices = c("select" = "", levels(soilFieldChem$selection1)))
-                    
-                  })
-                  
-                  output$select_selection2 <- renderUI({
-                          choice_selection2 <- reactive({
-                            soilFieldChem %>%
-                              filter(selection1 == input$s1) %>%
-                              pull(selection2) %>%
-                              as.character()
-                            
-                          })
-                        })
-                        
-                        output$selection4 <- renderUI({
-                          
-                          choice_selection4 <- reactive({
-                            soilFieldChem %>%
-                              filter(selection1 == input$s1) %>%
-                              filter(selection2 == input$s2) %>%
-                              pull(selection4) %>%
-                              as.character()
-                            
-                          })
-                        })
-                        <<<<<<< HEAD
-                        
-                        output$distPlot <- renderPlot({
-                          =======
-                            
-                            output$distPlot <- renderPlot({ 
-                              
-                              >>>>>>> parent of 8edd20a... update .groups error
-                              if (input$selection == "All Sites") {
-                                x    <-   soilFieldChem
-                              }
-                              else if (input$selection == "Grassland Sites" ) {
-                                x    <-  grass
-                              }
-                              else if (input$selection == "Forrested Sites" ) {
-                                x    <-  forest
-                              }
-                              <<<<<<< HEAD
-                              
-                              =======
-                                
-                                
-                                >>>>>>> parent of 8edd20a... update .groups error
-                              ggplot(x, aes(x=siteID, y= !!sym(input$selection3))) +
-                                geom_boxplot() +
-                                ylim(c(-20, 50)) +
-                                theme(axis.text.x = element_text(angle=45)) +
-                                ggtitle("for Each Site ID")
-                              
-                              
-                              
-                            })
-                          output$distPlot2 <- renderPlot ({
-                            if (input$selection == "All Sites") {
-                              x    <-   soilFieldChem
-                            }
-                            else if (input$selection == "Grassland Sites" ) {
-                              x    <-  grass
-                            }
-                            else if (input$selection == "Forrested Sites" ) {
-                              x    <-  forest
-                            }
-                            
-                            xsub <- x %>%
-                              group_by(siteID, nlcdClass) %>%
-                              summarise(mean_soilMoisture = mean(soilMoisture, na.rm = TRUE),
-                                        <<<<<<< HEAD
-                                        mean_soilTemp = mean(soilTemp, na.rm = TRUE), .groups="keep")
-                            
-                            g1 <- ggplot(xsub, aes(x=mean_soilMoisture, y=mean_soilTemp, color = siteID, label = siteID)) +
-                              =======
-                              mean_soilTemp = mean(soilTemp, na.rm = TRUE)) 
-                          
-                          g1 <- ggplot(xsub, aes(x=mean_soilMoisture, y=mean_soilTemp, color = siteID, label = siteID)) + 
-                            >>>>>>> parent of 8edd20a... update .groups error
-                          geom_point() + geom_text(aes(label=siteID),hjust=-0.2, vjust=0.5) +
-                            xlim(c(0, 4)) +
-                            ylim(c(0, 30)) +
-                            ggtitle('Soil Moisture x Temperatue Forested Plots')
-                          <<<<<<< HEAD
-                          <<<<<<< HEAD
-                          g1
-                          =======
-                            g1 
-                          >>>>>>> parent of 373efc5... Table less columns and WIP for graphs
-                          })
-                        }
-                        # Create Shiny app objects from either an explicit UI/server pair
-                        shinyApp(ui = ui, server = server)
-                        =======
-                          g1  
-                      })
-                  }
-                  
-                  
+  output$select_s1 <- renderUI({
+    selectizeInput('s1', 'Select variable 1', 
+                   choices = c("select" = "", unique(soilFieldChem$selection1)))
+  })
+  
+  output$select_selection2 <- renderUI({
+    choice_selection2 <- reactive({
+      soilFieldChem %>%
+        dplyr::filter(selection1 == input$s1) %>%
+        dplyr::pull(selection2) %>%
+        as.character()
+      })
+  })
+  
+  output$selection4 <- renderUI({
+    choice_selection4 <- reactive({
+      soilFieldChem %>%
+        filter(selection1 == input$s1) %>%
+        filter(selection2 == input$s2) %>%
+        pull(selection4) %>%
+        as.character()
+      })
+  })
+
+  output$distPlot <- renderPlot({
+        if (input$selection == "All Sites") { x <- soilFieldChem }
+        else if (input$selection == "Grassland Sites" ) { x <- grass }
+        else if (input$selection == "Forrested Sites" ) { x <- forest }
+
+    ggplot(data = x, aes(x=siteID, y= !!sym(input$selection3))) +
+      geom_boxplot() +
+      ylim(c(-20, 50)) +
+      theme(axis.text.x = element_text(angle=45)) +
+      ggtitle("for Each Site ID")
+    })
+  
+    output$distPlot2 <- renderPlot ({
+      if (input$selection == "All Sites") {  x <- soilFieldChem }
+      else if (input$selection == "Grassland Sites" ) { x <- grass }
+      else if (input$selection == "Forrested Sites" ) { x <- forest }
+      
+      xsub <- x %>%
+        group_by(siteID, nlcdClass) %>%
+        summarise(mean_soilMoisture = mean(soilMoisture, na.rm = TRUE),
+                  mean_soilTemp = mean(soilTemp, na.rm = TRUE), .groups="keep")
+      
+      g1 <- ggplot(data = xsub, aes(x=mean_soilMoisture, y=mean_soilTemp, color = siteID, label = siteID)) +
+        geom_point() + geom_text(aes(label=siteID),hjust=-0.2, vjust=0.5) +
+        xlim(c(0, 4)) +
+        ylim(c(0, 30)) +
+        ggtitle('Soil Moisture x Temperatue Forested Plots')
+    })
+}
+
 # Create Shiny app objects from either an explicit UI/server pair 
 shinyApp(ui = ui, server = server)
 
